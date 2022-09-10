900dfeca313afb8809242b7245d1e62cf230e40d
jhonarendra <jhonarendra@gmail.com>
2022-09-10 12:20:46 +0800

    add: html-react-parser for note body

--- src/pages/notes/_id.jsx
+++ src/pages/notes/_id.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import parser from 'html-react-parser'
 import { useParams } from 'react-router-dom'
+import { showFormattedDate } from '../../utils'
+import { getNote } from '../../utils/local-data'
 
 export default function NotesIdPages() {
+  const [note, setNote] = useState({})
   const { id } = useParams()
+
+  useEffect(() => {
+    setNote(getNote(id))
+  }, [])
   return (
-    <p>
-      Notes ID:
-      {' '}
-      {id}
-    </p>
+    <secion className="detail-page">
+      { 'id' in note ? (
+        <>
+          <h3 className="detail-page__title">
+            { note.title }
+          </h3>
+          <p className="detail-page__createdAt">
+            {showFormattedDate(note.createdAt)}
+          </p>
+          <div className="detail-page__body">
+            { parser(note.body) }
+          </div>
+        </>
+      ) : (
+        <p>Data tidak ditemukan</p>
+      )}
+
+    </secion>
   )
 }
--- src/utils/local-data.js
+++ src/utils/local-data.js
@@ -2,7 +2,7 @@ let notes = [
   {
     id: 'notes-1',
     title: 'Babel',
-    body: 'Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.',
+    body: '<b>Babel</b> merupakan <i>tools open-source</i> yang digunakan untuk mengubah sintaks <a href="https://en.wikipedia.org/wiki/ECMAScript">ECMAScript</a> 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.',
     createdAt: '2022-04-14T04:27:34.572Z',
     archived: false
   },

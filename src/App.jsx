c31af35a974a17711896f0f8fa1b48d3e7791eff
jhonarendra <jhonarendra@gmail.com>
2022-09-10 11:05:58 +0800

    feat: component ThemeToggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {
   Link, Navigate, Route, Routes
 } from 'react-router-dom'
@@ -8,26 +8,9 @@ import NotFoundPages from './pages/not-found'
 import ArchivesPage from './pages/archives'
 import NotesIdPages from './pages/notes/_id'
 import NotesNewPages from './pages/notes/new'
-import { capitalizeFirstLetter } from './utils'
+import ThemeToggler from './components/layout/ThemeToggler'
 
 function App() {
-  const [theme, setTheme] = useState('dark')
-
-  const changeTheme = (val) => {
-    setTheme(val)
-    const root = window.document.documentElement
-    root.setAttribute('data-theme', val)
-    localStorage.setItem('theme', val)
-  }
-
-  useEffect(() => {
-    if (localStorage.theme) {
-      changeTheme(localStorage.theme)
-    } else {
-      localStorage.setItem('theme', 'dark')
-      changeTheme('dark')
-    }
-  }, [])
   return (
     <div className="app-container">
       <header>
@@ -38,11 +21,7 @@ function App() {
           <ul>
             <li><Link to="/archives" title="Archives">Arsip</Link></li>
             <li>
-              <button type="button" onClick={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}>
-                {capitalizeFirstLetter(theme)}
-                {' '}
-                Theme
-              </button>
+              <ThemeToggler />
             </li>
           </ul>
         </nav>

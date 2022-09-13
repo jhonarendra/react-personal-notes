97604fefee21992f03548901ce7b0e65033265ab
jhonarendra <jhonarendra@gmail.com>
2022-09-13 22:01:03 +0800

    add: search

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import HomepageAction from '../components/index/HomePageAction'
 import NotesList from '../components/notes/NotesList'
-import { getAllNotes } from '../utils/local-data'
+import { getActiveNotes } from '../utils/local-data'
 
 export default function IndexPage() {
   const [notes, setNotes] = useState([])
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
 
   useEffect(() => {
-    setNotes(getAllNotes())
+    setNotes(getActiveNotes())
   }, [])
 
+  useEffect(() => {
+    if (search !== '') {
+      setNotes(
+        getActiveNotes()
+          .filter((note) => note.title.toLowerCase().includes(search.toLowerCase()))
+      )
+    } else {
+      setNotes(getActiveNotes())
+    }
+  }, [search])
   return (
     <section className="homepage">
       <h2>Catatan Aktif</h2>
       <section className="search-bar">
-        <input type="text" placeholder="Cari berdasarkan judul ..." value="" />
+        <input
+          type="text"
+          placeholder="Cari berdasarkan judul ..."
+          value={search}
+          onChange={handleSearch}
+        />
       </section>
-      <NotesList notes={notes} />
+      {notes.length > 0 && <NotesList notes={notes} />}
+      {notes.length === 0 && <p>Tidak ada catatan</p>}
       <HomepageAction />
     </section>
   )
